b73ba1abcb02d224866fecacbaeafa9f81c0c590
Jumpei Matsuda <jmatsu.drm@gmail.com>
2022-06-26 04:53:32 +0900

    feat: Use sdk v2 api in resource usergroup's default channels

--- slack/resource_usergroup_channels.go
+++ slack/resource_usergroup_channels.go
@@ -3,6 +3,7 @@ package slack
 import (
 	"context"
 	"fmt"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"github.com/slack-go/slack"
 	"log"
@@ -10,15 +11,15 @@ import (
 
 func resourceSlackUserGroupChannels() *schema.Resource {
 	return &schema.Resource{
-		Read:   resourceSlackUserGroupChannelsRead,
-		Create: resourceSlackUserGroupChannelsCreate,
-		Update: resourceSlackUserGroupChannelsUpdate,
-		Delete: resourceSlackUserGroupChannelsDelete,
+		ReadContext:   resourceSlackUserGroupChannelsRead,
+		CreateContext: resourceSlackUserGroupChannelsCreate,
+		UpdateContext: resourceSlackUserGroupChannelsUpdate,
+		DeleteContext: resourceSlackUserGroupChannelsDelete,
 
 		Importer: &schema.ResourceImporter{
-			State: func(d *schema.ResourceData, m interface{}) ([]*schema.ResourceData, error) {
+			StateContext: func(ctx context.Context, d *schema.ResourceData, m interface{}) ([]*schema.ResourceData, error) {
 				_ = d.Set("usergroup_id", d.Id())
-				return schema.ImportStatePassthrough(d, m)
+				return schema.ImportStatePassthroughContext(ctx, d, m)
 			},
 		},
 
@@ -43,10 +44,8 @@ func configureSlackUserGroupChannels(d *schema.ResourceData, userGroup slack.Use
 	_ = d.Set("channels", append(userGroup.Prefs.Channels, userGroup.Prefs.Groups...))
 }
 
-func resourceSlackUserGroupChannelsCreate(d *schema.ResourceData, meta interface{}) error {
+func resourceSlackUserGroupChannelsCreate(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
 	client := meta.(*Team).client
-
-	ctx := context.WithValue(context.Background(), ctxId, d.Id())
 
 	usergroupId := d.Get("usergroup_id").(string)
 	log.Printf("[DEBUG] Creating usergroup channels relation: %s", usergroupId)
@@ -67,7 +66,13 @@ func resourceSlackUserGroupChannelsCreate(d *schema.ResourceData, meta interface
 	userGroup, err := client.UpdateUserGroupContext(ctx, *params)
 
 	if err != nil {
-		return err
+		return diag.Diagnostics{
+			{
+				Severity: diag.Error,
+				Summary:  fmt.Sprintf("provider cannot add the default channels to the slack usergroup (%s)", usergroupId),
+				Detail:   err.Error(),
+			},
+		}
 	}
 
 	configureSlackUserGroupChannels(d, userGroup)
@@ -75,16 +80,20 @@ func resourceSlackUserGroupChannelsCreate(d *schema.ResourceData, meta interface
 	return nil
 }
 
-func resourceSlackUserGroupChannelsRead(d *schema.ResourceData, meta interface{}) error {
+func resourceSlackUserGroupChannelsRead(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
 	client := meta.(*Team).client
-
-	ctx := context.WithValue(context.Background(), ctxId, d.Id())
 
 	usergroupId := d.Get("usergroup_id").(string)
 	log.Printf("[DEBUG] Reading usergroup channels relation: %s", usergroupId)
 
 	if usergroupId != d.Id() {
-		return fmt.Errorf("it looks usergroup id has been changed but it's not allowed. Res ID: %s", d.Id())
+		return diag.Diagnostics{
+			{
+				Severity: diag.Error,
+				Summary:  fmt.Sprintf("it's not allowed to change usergroup id (from %s to %s)", d.Id, usergroupId),
+				Detail:   "Please move the state or create another resource instead",
+			},
+		}
 	}
 
 	// Use a cache for usergroups api call because the limitation is strict
@@ -98,7 +107,13 @@ func resourceSlackUserGroupChannelsRead(d *schema.ResourceData, meta interface{}
 		})
 
 		if err != nil {
-			return err
+			return diag.Diagnostics{
+				{
+					Severity: diag.Error,
+					Summary:  fmt.Sprintf("provider cannot read the default channels of the slack usergroup (%s)", usergroupId),
+					Detail:   err.Error(),
+				},
+			}
 		}
 
 		userGroups = &tempUserGroups
@@ -107,7 +122,13 @@ func resourceSlackUserGroupChannelsRead(d *schema.ResourceData, meta interface{}
 	}
 
 	if userGroups == nil {
-		panic(fmt.Errorf("a serious error happened. please create an issue to https://github.com/jmatsu/terraform-provider-slack"))
+		return diag.Diagnostics{
+			{
+				Severity: diag.Error,
+				Summary:  fmt.Sprintf("Serious error happened while reading default channles of the slack usergroup (%s)", usergroupId),
+				Detail:   "Internal provider error. Please open an issue at https://github.com/jmatsu/terraform-provider-slack",
+			},
+		}
 	}
 
 	for _, userGroup := range *userGroups {
@@ -120,16 +141,20 @@ func resourceSlackUserGroupChannelsRead(d *schema.ResourceData, meta interface{}
 	return nil
 }
 
-func resourceSlackUserGroupChannelsUpdate(d *schema.ResourceData, meta interface{}) error {
+func resourceSlackUserGroupChannelsUpdate(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
 	client := meta.(*Team).client
-
-	ctx := context.WithValue(context.Background(), ctxId, d.Id())
 
 	usergroupId := d.Get("usergroup_id").(string)
 	log.Printf("[DEBUG] Updating usergroup channels relation: %s", usergroupId)
 
 	if usergroupId != d.Id() {
-		return fmt.Errorf("it looks usergroup id has been changed but it's not allowed. Res ID: %s", d.Id())
+		return diag.Diagnostics{
+			{
+				Severity: diag.Error,
+				Summary:  fmt.Sprintf("it's not allowed to change usergroup id (from %s to %s)", d.Id, usergroupId),
+				Detail:   "Please move the state or create another resource instead",
+			},
+		}
 	}
 
 	iChannels := d.Get("channels").(*schema.Set).List()
@@ -148,7 +173,13 @@ func resourceSlackUserGroupChannelsUpdate(d *schema.ResourceData, meta interface
 	userGroup, err := client.UpdateUserGroupContext(ctx, *params)
 
 	if err != nil {
-		return err
+		return diag.Diagnostics{
+			{
+				Severity: diag.Error,
+				Summary:  fmt.Sprintf("provider cannot update the default channels of the slack usergroup (%s)", usergroupId),
+				Detail:   err.Error(),
+			},
+		}
 	}
 
 	configureSlackUserGroupChannels(d, userGroup)
@@ -156,16 +187,21 @@ func resourceSlackUserGroupChannelsUpdate(d *schema.ResourceData, meta interface
 	return nil
 }
 
-func resourceSlackUserGroupChannelsDelete(d *schema.ResourceData, meta interface{}) error {
+func resourceSlackUserGroupChannelsDelete(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
 	client := meta.(*Team).client
 
-	ctx := context.WithValue(context.Background(), ctxId, d.Id())
 	usergroupId := d.Get("usergroup_id").(string)
 
 	log.Printf("[DEBUG] Deleting usergroup channels relation: %s", usergroupId)
 
 	if usergroupId != d.Id() {
-		return fmt.Errorf("it looks usergroup id has been changed but it's not allowed. Res ID: %s", d.Id())
+		return diag.Diagnostics{
+			{
+				Severity: diag.Error,
+				Summary:  fmt.Sprintf("it's not allowed to change usergroup id (from %s to %s)", d.Id, usergroupId),
+				Detail:   "Please move the state or create another resource instead",
+			},
+		}
 	}
 
 	params := &slack.UserGroup{
@@ -175,8 +211,17 @@ func resourceSlackUserGroupChannelsDelete(d *schema.ResourceData, meta interface
 		},
 	}
 
-	_, err := client.UpdateUserGroupContext(ctx, *params)
+	if _, err := client.UpdateUserGroupContext(ctx, *params); err != nil {
+		return diag.Diagnostics{
+			{
+				Severity: diag.Error,
+				Summary:  fmt.Sprintf("provider cannot remove all default channels from the slack usergroup (%s)", usergroupId),
+				Detail:   err.Error(),
+			},
+		}
+	}
 
 	d.SetId("")
-	return err
+
+	return nil
 }
